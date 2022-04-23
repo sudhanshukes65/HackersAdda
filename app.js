2399e3e327b0012b94055be4631d60c0c98d317e
SUDHANSHU KESARWANI <79747228+sudhanshukes65@users.noreply.github.com>
2022-04-23 21:25:01 +0530

    adding search fucntion

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const SearchBoxFunction = () => {
-    let filter = document.getElementById('searchBox').value ;
+    let filter = document.getElementById('searchBox').value.toUpperCase() ;
+    let ul = document.getElementById('myUL');
+    let li = ul.getElementsByTagName('li');
     
+    for(var i=0;i<li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
+
+        let textValue = a.innerText.toUpperCase();
+
+        if(textValue.indexOf(filter)>-1){
+            li[i].style.display = '';
+        }
+        else{
+            li[i].style.display = 'none';
+        }
+         
+    }
+
+
 }
